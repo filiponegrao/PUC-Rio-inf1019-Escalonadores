932759fede2c493bb3636148e90aa10f871edaf5
filiponegrao <filiposilver@hotmail.com>
2016-10-18 11:32:00 -0200

    arrumando a pasta

---
+++ Trabalho1/trabalho1.c
@@ -0,0 +1,17 @@
+
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h> 
+
+
+int main (int argc, char* argv[] ) {
+
+	
+}
