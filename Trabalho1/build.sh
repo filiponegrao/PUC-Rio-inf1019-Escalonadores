0cacf255500c494c105105198d421c6a2c95e1df
filiponegrao <filiposilver@hotmail.com>
2016-10-24 23:51:30 -0200

    foi

--- Trabalho1/build.sh
+++ Trabalho1/build.sh
@@ -1,14 +1,18 @@
 cd src
 gcc trabalho1.c trabalho1.h -o trab1
 cp trab1 ../trab1
+rm trab1
 
 cd Processos
 
 gcc processo1.c -o processo1
 cp processo1 ../../processo1
+rm processo1
 
 gcc processo2.c -o processo2
 cp processo2 ../../processo2
+rm processo2
 
 gcc processo3.c -o processo3
 cp processo3 ../../processo3
+rm processo3
